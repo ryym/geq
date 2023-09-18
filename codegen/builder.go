44c71571edb95dfebc3d53b17130525562b218b0
ryym <ryym.64@gmail.com>
2023-09-18 18:23:36 +0900

    refactor: Stop auto-generate basic query helper functions

--- codegen/builder.go
+++ codegen/builder.go
@@ -48,11 +48,6 @@ type relshipDef struct {
 	FieldType string
 }
 
-type builderGeqFileDef struct {
-	PkgName string
-	Import  string
-}
-
 func genBuilderFile(rootPath string, pkg *packages.Package) (err error) {
 	cfg, err := parseBuilderConfig(pkg)
 	if err != nil {
@@ -68,19 +63,6 @@ func genBuilderFile(rootPath string, pkg *packages.Package) (err error) {
 	err = os.MkdirAll(destDir, os.ModePerm)
 	if err != nil {
 		return fmt.Errorf("failed to create package directory: %w", err)
-	}
-
-	geqDef := &builderGeqFileDef{
-		PkgName: def.PkgName,
-		Import:  geqPkgPath,
-	}
-	src, err := buildGoCode("builderGeqFile", builderGeqFileTmpl, geqDef)
-	if err != nil {
-		return err
-	}
-	err = writeFile(destDir, "geq.gen.go", src)
-	if err != nil {
-		return err
 	}
 
 	if len(def.Tables) > 0 {
@@ -363,102 +345,4 @@ func (t *Table{{.Name}}) As(alias string) *Table{{.Name}} {
 
 {{end}}
 
-`
-
-const builderGeqFileTmpl = `
-package {{.PkgName}}
-
-import "{{.Import}}"
-
-func AsMap[R any, K comparable](key *geq.Column[K], q *geq.Query[R]) *geq.MapLoader[R, R, K] {
-	return geq.Builder_AsMap(key, q)
-}
-
-func AsSliceMap[R any, K comparable](key *geq.Column[K], q *geq.Query[R]) *geq.SliceMapLoader[R, R, K] {
-	return geq.Builder_AsSliceMap(key, q)
-}
-
-func ToSlice[R any](mapper geq.RowMapper[R], dest *[]R) *geq.SliceScanner[R] {
-	return geq.Builder_ToSlice(mapper, dest)
-}
-
-func ToMap[R any, K comparable](mapper geq.RowMapper[R], key geq.TypedSelection[K], dest *map[K]R) *geq.MapScanner[R, K] {
-	return geq.Builder_ToMap(mapper, key, dest)
-}
-
-func ToSliceMap[R any, K comparable](mapper geq.RowMapper[R], key geq.TypedSelection[K], dest *map[K][]R) *geq.SliceMapScanner[R, K] {
-	return geq.Builder_ToSliceMap(mapper, key, dest)
-}
-
-func SelectFrom[R any](table geq.Table[R], sels ...geq.Selection) *geq.Query[R] {
-	return geq.Builder_SelectFrom(table, sels...)
-}
-
-func SelectAs[R any](mapper geq.RowMapper[R]) *geq.Query[R] {
-	return geq.Builder_SelectAs(mapper)
-}
-
-func SelectOnly[V any](col *geq.Column[V]) *geq.Query[V] {
-	return geq.Builder_SelectOnly(col)
-}
-
-func Select(sels ...geq.Selection) *geq.Query[struct{}] {
-	return geq.Builder_Select(sels...)
-}
-
-func SelectVia[S, T, C any](srcs []S, from geq.Table[T], relship *geq.Relship[S, C]) *geq.Query[T] {
-	return geq.Builder_SelectVia(srcs, from, relship)
-}
-
-func InsertInto(table geq.AnyTable) *geq.InsertQuery {
-	return geq.Builder_InsertInto(table)
-}
-
-func Update(table geq.AnyTable) *geq.UpdateQuery {
-	return geq.Builder_Update(table)
-}
-
-func Null() geq.AnonExpr {
-	return geq.Builder_Null()
-}
-
-func Concat(vals ...any) geq.AnonExpr {
-	return geq.Builder_Concat(vals...)
-}
-
-func Func(name string, args ...any) geq.AnonExpr {
-	return geq.Builder_Func(name, args...)
-}
-
-func Count(expr geq.Expr) geq.AnonExpr {
-	return Func("COUNT", expr)
-}
-
-func Sum(expr geq.Expr) geq.AnonExpr {
-	return Func("SUM", expr)
-}
-
-func Min(expr geq.Expr) geq.AnonExpr {
-	return Func("MIN", expr)
-}
-
-func Max(expr geq.Expr) geq.AnonExpr {
-	return Func("MAX", expr)
-}
-
-func Avg(expr geq.Expr) geq.AnonExpr {
-	return Func("AVG", expr)
-}
-
-func Coalesce(exprs ...geq.Expr) geq.AnonExpr {
-	vals := make([]any, 0, len(exprs))
-	for _, e := range exprs {
-		vals = append(vals, e)
-	}
-	return Func("COALESCE", vals...)
-}
-
-func Raw(sql string) *geq.RawExpr {
-	return geq.Builder_Raw(sql)
-}
 `
